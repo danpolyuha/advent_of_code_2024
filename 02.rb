eb4f3e29fe4997d10c56075e15712da9acdd2bca
DanPolyuha <dpolyuha@gmail.com>
2024-12-07 13:59:25 +0100

    02

--- 02.rb
+++ 02.rb
@@ -6,7 +6,7 @@ matrix = File.readlines('input02.txt').map { |line| line.split.map(&:to_i) }
 
 result = matrix.count do |line|
   diffs = line.each_cons(2).map { |a, b| b - a }
-  diffs.all? { |item| item.in?(-3..-1) } || diffs.all? { |n| n.in?(1..3) }
+  diffs.all? { |diff| diff.in?(-3..-1) } || diffs.all? { |diff| diff.in?(1..3) }
 end
 
 puts result
@@ -19,7 +19,7 @@ matrix = File.readlines('input02.txt').map { |line| line.split.map(&:to_i) }
 
 def ok?(array)
   diffs = array.each_cons(2).map { |a, b| b - a }
-  diffs.all? { |item| item.in?(-3..-1) } || diffs.all? { |n| n.in?(1..3) }
+  diffs.all? { |diff| diff.in?(-3..-1) } || diffs.all? { |diff| diff.in?(1..3) }
 end
 
 result = matrix.count do |line|
