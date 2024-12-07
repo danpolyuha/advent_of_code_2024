c2aee28026edfcd8d3011d0105041abd71116448
DanPolyuha <dpolyuha@gmail.com>
2024-12-07 20:53:27 +0100

    05

--- 05.rb
+++ 05.rb
@@ -2,63 +2,40 @@
 # 1
 #############################################################################
 
-lines = File.readlines('tmp/adventofcode2024/input05.txt')
+lines = File.readlines('input05.txt')
 first_update_index = lines.find_index { |line| line.include?(',') }
 rules = lines[..first_update_index - 2].map { |line| line.split('|').map(&:to_i) }
 updates = lines[first_update_index..].map { |line| line.split(',').map(&:to_i) }
 
-rule_hash = {}
-rules.each do |items|
-  rule_hash[items[0]] ||= Set.new
-  rule_hash[items[0]] << items[1]
-end
+@rule_hash = Hash.new { |h, k| h[k] = Set.new }
+rules.each { |items| @rule_hash[items[0]] << items[1] }
 
-def ok?(update, rule_hash)
-  (0..update.length - 2).each do |start|
-    (start + 1..update.length - 1).each do |current|
-      return false if rule_hash[update[current]]&.include?(update[start])
-    end
+def ok?(update)
+  update == update.sort do |a, b|
+    @rule_hash[a].include?(b) ? -1 : (@rule_hash[b].include?(a) ? 1 : 0)
   end
 end
 
-result = updates.select { |update| ok?(update, rule_hash) }.sum { |update| update[update.length / 2] }
-puts result
+puts updates.select { |update| ok?(update) }.sum { |update| update[update.length / 2] }
 
 #############################################################################
 # 2
 #############################################################################
 
-lines = File.readlines('tmp/adventofcode2024/input05.txt')
+lines = File.readlines('input05.txt')
 first_update_index = lines.find_index { |line| line.include?(',') }
 rules = lines[..first_update_index - 2].map { |line| line.split('|').map(&:to_i) }
 updates = lines[first_update_index..].map { |line| line.split(',').map(&:to_i) }
 
-rule_hash = {}
-rules.each do |items|
-  rule_hash[items[0]] ||= Set.new
-  rule_hash[items[0]] << items[1]
-end
+@rule_hash = Hash.new { |h, k| h[k] = Set.new }
+rules.each { |items| @rule_hash[items[0]] << items[1] }
 
-def fix?(update, rule_hash)
-  result = false
-
-  (0..update.length - 2).each do |start|
-    loop do
-      switched = false
-      (start + 1..update.length - 1).each do |current|
-        if rule_hash[update[current]]&.include?(update[start])
-          switched = true
-          update[start], update[current] = update[current], update[start]
-          break
-        end
-      end
-
-      switched ? (result = true) : break
-    end
+def fix?(update)
+  sorted_update = update.sort do |a, b|
+    @rule_hash[a].include?(b) ? -1 : (@rule_hash[b].include?(a) ? 1 : 0)
   end
 
-  result
+  sorted_update if sorted_update != update
 end
 
-result = updates.select { |update| fix?(update, rule_hash) }.sum { |update| update[update.length / 2] }
-puts result
+puts updates.filter_map { |update| fix?(update) }.sum { |update| update[update.length / 2] }
