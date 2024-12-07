2aed1db31c35dcab3bb8d0dde2f2d56ad07afada
DanPolyuha <dpolyuha@gmail.com>
2024-12-07 14:16:18 +0100

    03

--- 03.rb
+++ 03.rb
@@ -2,32 +2,23 @@
 # 1
 #############################################################################
 
-string = File.read('tmp/adventofcode2024/input03.txt')
+input = File.read('input03.txt')
 
-result = string.scan(/mul\((\d{1,3})\,(\d{1,3})\)/).sum do |item|
-  item[0].to_i * item[1].to_i
-end
-
-puts result
+regex = /mul\((\d{1,3})\,(\d{1,3})\)/
+puts input.scan(regex).sum { |mults| mults[0].to_i * mults[1].to_i }
 
 #############################################################################
 # 2
 #############################################################################
 
-string = File.read('tmp/adventofcode2024/input03.txt')
+input = File.read('input03.txt')
 
 result = 0
 enabled = true
-string.scan(/mul\((\d{1,3})\,(\d{1,3})\)|(don't\(\))|(do\(\))/) do |item|
-  if item[2] == "don't()"
-    enabled = false
-    next
-  elsif item[3] == 'do()'
-    enabled = true
-    next
-  else
-    result += item[0].to_i * item[1].to_i if enabled
-  end
+input.scan(/mul\((\d{1,3})\,(\d{1,3})\)|(don't\(\)|do\(\))/) do |items|
+  enabled = false and next if items[2] == "don't()"
+  enabled = true and next if items[2] == 'do()'
+  result += items[0].to_i * items[1].to_i if enabled
 end
 
 puts result
