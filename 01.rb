69f8080f550710019b809fd17f634d63288e6875
DanPolyuha <dpolyuha@gmail.com>
2024-12-24 14:30:29 +0100

    updates01

--- 01.rb
+++ 01.rb
@@ -8,7 +8,7 @@ loc_id_lists = input_lines.map { |line| line.split.map(&:to_i) }.transpose
 loc_ids1_sorted = loc_id_lists[0].sort
 loc_ids2_sorted = loc_id_lists[1].sort
 
-puts loc_ids1_sorted.each_with_index.sum { |item, index| (item - loc_ids2_sorted[index]).abs }
+puts loc_ids1_sorted.each_with_index.sum { |id1, index| (id1 - loc_ids2_sorted[index]).abs }
 # 1882714
 
 ########################################################################################################################
