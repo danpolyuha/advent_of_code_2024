fe08f09531a719664e250a384c678d76aa87ff26
DanPolyuha <dpolyuha@gmail.com>
2024-12-25 14:00:58 +0100

    updates09

--- 09.rb
+++ 09.rb
@@ -1,60 +1,62 @@
-#############################################################################
+disk_map = File.read('input09.txt').chars.map(&:to_i)
+
+########################################################################################################################
 # 1 I'm sure there is a better way to do this, but I don't have time
-#############################################################################
+########################################################################################################################
 
-items = File.read('input09.txt').chars.map(&:to_i)
-
-unzipped = []
-items.each_with_index do |item, index|
+unzipped_map = []
+disk_map.each_with_index do |length, index|
   value = index.even? ? [index / 2] : [-1]
-  unzipped += value * item
+  unzipped_map += value * length
 end
 
 index_to_relocate = 0
 
 loop do
-  last_value_index = unzipped.rindex { |item| item != -1 }
-  unzipped.slice!(last_value_index + 1..)
+  last_block_cell_index = unzipped_map.rindex { |item| item != -1 }
+  unzipped_map.slice!(last_block_cell_index + 1..)
 
-  break if index_to_relocate >= unzipped.length
+  break if index_to_relocate >= unzipped_map.length
 
-  unzipped[index_to_relocate] = unzipped.delete_at(unzipped.length - 1) if unzipped[index_to_relocate] == -1
+  if unzipped_map[index_to_relocate] == -1
+    unzipped_map[index_to_relocate] = unzipped_map.delete_at(unzipped_map.length - 1)
+  end
+
   index_to_relocate += 1
 end
 
-puts unzipped.each_with_index.sum { |item, index| item * index }
+puts unzipped_map.each_with_index.sum { |item, index| item * index }
+# 6398608069280
 
-#############################################################################
+########################################################################################################################
 # 2
-#############################################################################
+########################################################################################################################
 
-items = File.read('input09.txt').chars.map(&:to_i)
-
-structure = []
-items.each_with_index do |item, index|
-  structure << (index.even? ? [index / 2, item] : [-1, item]) if item > 0
+block_structure = []
+disk_map.each_with_index do |length, index|
+  block_structure << (index.even? ? [index / 2, length] : [-1, length]) if length > 0
 end
 
-index_to_relocate = structure.length - 1
+index_to_relocate = block_structure.length - 1
 while index_to_relocate >= 0
-  item_to_relocate = structure[index_to_relocate]
+  item_to_relocate = block_structure[index_to_relocate]
 
   if item_to_relocate[0] != -1
-    free_space_index = structure[..index_to_relocate].find_index do |item|
-      item[0] == -1 && item[1] >= item_to_relocate[1]
+    free_space_index = block_structure[..index_to_relocate].find_index do |value, length|
+      value == -1 && length >= item_to_relocate[1]
     end
 
     if free_space_index
-      structure.insert(free_space_index, item_to_relocate.dup)
+      block_structure.insert(free_space_index, item_to_relocate.dup)
       index_to_relocate += 1
-      structure[free_space_index + 1][1] -= item_to_relocate[1]
-      structure[index_to_relocate][0] = -1
+      block_structure[free_space_index + 1][1] -= item_to_relocate[1]
+      block_structure[index_to_relocate][0] = -1
     end
   end
 
   index_to_relocate -= 1
 end
 
-unzipped = []
-structure.each_with_index { |item| unzipped += [item[0]] * item[1] }
-puts unzipped.each_with_index.sum { |item, index| item != -1 ? item * index : 0 }
+unzipped_map = block_structure.sum([]) { |value, length| [value] * length }
+puts unzipped_map.each_with_index.sum { |item, index| item != -1 ? item * index : 0 }
+# 6427437134372
