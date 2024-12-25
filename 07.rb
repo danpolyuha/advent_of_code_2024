12e9b591764759a4d146b538c962aa4dd9c9ca21
DanPolyuha <dpolyuha@gmail.com>
2024-12-25 01:18:09 +0100

    updates07

--- 07.rb
+++ 07.rb
@@ -1,53 +1,51 @@
-#############################################################################
+equations = File.readlines('input07.txt').map { |line| line.scan(/\d+/).map(&:to_i) }
+
+########################################################################################################################
 # 1 Both parts can be optimized by generation of the permutations manually, but it's good enough for the input
-#############################################################################
+########################################################################################################################
 
-lines = File.readlines('input07.txt')
-data = lines.map { |line| line.scan(/\d+/).map(&:to_i) }; 0
-
-def ok?(goal, items)
-  ['*', '+'].repeated_permutation(items.length - 1).each do |ops|
-    n = items[0]
+def equation_can_be_true?(goal, operands)
+  ['*', '+'].repeated_permutation(operands.length - 1).each do |ops|
+    result = operands[0]
     ops.each_with_index do |op, index|
-      op == '*' ? n += items[index + 1] : n *= items[index + 1]
-      break if n > goal
+      op == '*' ? result += operands[index + 1] : result *= operands[index + 1]
+      break if result > goal
     end
 
-    return true if n == goal
+    return true if result == goal
   end
 
   false
 end
 
-puts data.sum { |items| ok?(items[0], items[1..]) ? items[0] : 0 }
+puts equations.sum { |items| equation_can_be_true?(items[0], items[1..]) ? items[0] : 0 }
+# 20281182715321
 
-#############################################################################
+########################################################################################################################
 # 2
-#############################################################################
+########################################################################################################################
 
-lines = File.readlines('input07.txt')
-data = lines.map { |line| line.scan(/\d+/).map(&:to_i) }; 0
-
-def ok2?(goal, items)
-  ['*', '+', '|'].repeated_permutation(items.length - 1).each do |ops|
-    n = items[0]
+def equation_can_be_true2?(goal, operands)
+  ['*', '+', '|'].repeated_permutation(operands.length - 1).each do |ops|
+    result = operands[0]
     ops.each_with_index do |op, index|
       case op
       when '+'
-        n += items[index + 1]
+        result += operands[index + 1]
       when '*'
-        n *= items[index + 1]
+        result *= operands[index + 1]
       when '|'
-        n = "#{n}#{items[index + 1]}".to_i
+        result = "#{result}#{operands[index + 1]}".to_i
       end
 
-      break if n > goal
+      break if result > goal
     end
 
-    return true if n == goal
+    return true if result == goal
   end
 
   false
 end
 
-puts data.sum { |items| ok2?(items[0], items[1..]) ? items[0] : 0 }
+puts equations.sum { |items| equation_can_be_true2?(items[0], items[1..]) ? items[0] : 0 }
+# 159490400628354
