cbd6e3324a3f508455d1cffc6c630038dece27af
DanPolyuha <dpolyuha@gmail.com>
2024-12-26 14:15:44 +0100

    update12

--- 12.rb
+++ 12.rb
@@ -1,101 +1,90 @@
-#############################################################################
-# 1
-#############################################################################
+# https://adventofcode.com/2024/day/12
 
-@matrix = File.readlines('input12.txt').map(&:chomp).map(&:chars)
-@n = @matrix.length
-@m = @matrix[0].length
+@garden = File.readlines('input12.txt').map(&:chomp).map(&:chars)
+@height = @garden.length
+@width = @garden[0].length
 
-@processed_points = Set.new
-regions = []
-
-def process letter, y, x, region
-  return if y < 0 || y >= @n || x < 0 || x >= @m || @matrix[y][x] != letter || @processed_points.include?([y, x])
+def calculate_region(plant, y, x, region, processed_points)
+  return if y < 0 || y >= @height || x < 0 || x >= @width || @garden[y][x] != plant
+  return if processed_points.include?([y, x])
 
   region[:cells] << [y, x]
-  @processed_points << [y, x]
+  processed_points << [y, x]
 
-  process(letter, y - 1, x, region)
-  process(letter, y + 1, x, region)
-  process(letter, y, x - 1, region)
-  process(letter, y, x + 1, region)
+  calculate_region(plant, y - 1, x, region, processed_points)
+  calculate_region(plant, y + 1, x, region, processed_points)
+  calculate_region(plant, y, x - 1, region, processed_points)
+  calculate_region(plant, y, x + 1, region, processed_points)
 end
 
-@matrix.each_with_index do |line, y|
-  line.each_with_index do |item, x|
-    next if @processed_points.include?([y, x])
+def form_regions
+  processed_points = Set.new
+  regions = []
 
-    region = { letter: item, cells: [] }
-    regions << region
-    process(item, y, x, region)
+  @garden.each_with_index do |row, y|
+    row.each_with_index do |plant, x|
+      next if processed_points.include?([y, x])
+
+      region = { plant: plant, cells: [] }
+      regions << region
+      calculate_region(plant, y, x, region, processed_points)
+    end
   end
+
+  regions
 end
+
+########################################################################################################################
+# 1
+########################################################################################################################
+
+regions = form_regions
 
 regions.each do |region|
   perimeter = 0
   region[:cells].each do |y, x|
-    perimeter += 1 if y == 0 || @matrix[y - 1][x] != region[:letter]
-    perimeter += 1 if x == 0 || @matrix[y][x - 1] != region[:letter]
-    perimeter += 1 if y == @n - 1 || @matrix[y + 1][x] != region[:letter]
-    perimeter += 1 if x == @m - 1 || @matrix[y][x + 1] != region[:letter]
+    perimeter += 1 if y == 0 || @garden[y - 1][x] != region[:plant]
+    perimeter += 1 if x == 0 || @garden[y][x - 1] != region[:plant]
+    perimeter += 1 if y == @height - 1 || @garden[y + 1][x] != region[:plant]
+    perimeter += 1 if x == @width - 1 || @garden[y][x + 1] != region[:plant]
   end
 
   region[:perimeter] = perimeter
 end
 
 puts regions.sum { |r| r[:perimeter] * r[:cells].count }
+# 1319878
 
-#############################################################################
+########################################################################################################################
 # 2 This can be done wiser, but again, I don't have time
-#############################################################################
+########################################################################################################################
 
-@matrix = File.readlines('input12.txt').map(&:chomp).map(&:chars)
-@n = @matrix.length
-@m = @matrix[0].length
-
-@processed_points = Set.new
-regions = []
-
-def process2 letter, y, x, region
-  return if y < 0 || y >= @n || x < 0 || x >= @m || @matrix[y][x] != letter || @processed_points.include?([y, x])
-
-  region[:cells] << [y, x]
-  @processed_points << [y, x]
-  process2(letter, y - 1, x, region)
-  process2(letter, y + 1, x, region)
-  process2(letter, y, x - 1, region)
-  process2(letter, y, x + 1, region)
-end
-
-@matrix.each_with_index do |line, y|
-  line.each_with_index do |item, x|
-    next if @processed_points.include?([y, x])
-
-    region = { letter: item, cells: [] }
-    regions << region
-    process2(item, y, x, region)
-  end
-end
+regions = form_regions
 
 regions.each do |region|
-  top_cells = region[:cells].select do |y, x|
+  top_edges = region[:cells].select do |y, x|
     !region[:cells].include?([y - 1, x]) &&
-      (!region[:cells].include?([y, x - 1]) || (region[:cells].include?([y, x - 1]) && region[:cells].include?([y - 1, x - 1])))
+      (!region[:cells].include?([y, x - 1]) ||
+        (region[:cells].include?([y, x - 1]) && region[:cells].include?([y - 1, x - 1])))
   end
-  bottom_cells = region[:cells].select do |y, x|
+  bottom_edges = region[:cells].select do |y, x|
     !region[:cells].include?([y + 1, x]) &&
-      (!region[:cells].include?([y, x - 1]) || (region[:cells].include?([y, x - 1]) && region[:cells].include?([y + 1, x - 1])))
+      (!region[:cells].include?([y, x - 1]) ||
+        (region[:cells].include?([y, x - 1]) && region[:cells].include?([y + 1, x - 1])))
   end
-  left_cells = region[:cells].select do |y, x|
+  left_edges = region[:cells].select do |y, x|
     !region[:cells].include?([y, x - 1]) &&
-      (!region[:cells].include?([y - 1, x]) || (region[:cells].include?([y - 1, x]) && region[:cells].include?([y - 1, x - 1])))
+      (!region[:cells].include?([y - 1, x]) ||
+        (region[:cells].include?([y - 1, x]) && region[:cells].include?([y - 1, x - 1])))
   end
-  right_cells = region[:cells].select do |y, x|
+  right_edges = region[:cells].select do |y, x|
     !region[:cells].include?([y, x + 1]) &&
-      (!region[:cells].include?([y - 1, x]) || (region[:cells].include?([y - 1, x]) && region[:cells].include?([y - 1, x + 1])))
+      (!region[:cells].include?([y - 1, x]) ||
+        (region[:cells].include?([y - 1, x]) && region[:cells].include?([y - 1, x + 1])))
   end
 
-  region[:perimeter] = top_cells.count + bottom_cells.count + left_cells.count + right_cells.count
+  region[:perimeter] = top_edges.count + bottom_edges.count + left_edges.count + right_edges.count
 end
 
 puts regions.sum { |r| r[:perimeter] * r[:cells].count }
+# 784982
